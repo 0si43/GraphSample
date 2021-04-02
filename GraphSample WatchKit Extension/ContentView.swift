6dc4b6780b860e87724e750680ab5a2990744206
0si43 <symn43@gmail.com>
2021-04-02 17:59:42 +0900

    make List View for graph representation

--- GraphSample WatchKit Extension/ContentView.swift
+++ GraphSample WatchKit Extension/ContentView.swift
@@ -8,9 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    var number = 0
+    
     var body: some View {
-        Text("Hello, World!")
-            .padding()
+        List {
+            HStack {
+                Image(systemName: "person")
+                Spacer()
+                Text(String(number))
+            }
+            .listRowPlatterColor(.green)
+            HStack {
+                Image(systemName: "person")
+                Spacer()
+                Text(String(number))
+            }
+            .listRowPlatterColor(.blue)
+            HStack {
+                Image(systemName: "person")
+                Spacer()
+                Text(String(number))
+            }
+            .listRowPlatterColor(.red)
+            HStack {
+                Image(systemName: "person")
+                Spacer()
+                Text(String(number))
+            }
+            .listRowPlatterColor(.yellow)
+        }
+        .navigationBarTitle(Text("グラフ"))
     }
 }
 
